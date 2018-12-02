ea9a7486c01bcd891db396ee7f826f64c3c69a82
Roy Mustang <juliojose3000@gmail.com>
2018-12-02 15:02:03 -0600

    first commit

---
+++ app/src/main/java/cr/ac/ucr/teamjjja/sistemasoperativos/servercontroller/MainActivity.java
@@ -0,0 +1,13 @@
+package cr.ac.ucr.teamjjja.sistemasoperativos.servercontroller;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
